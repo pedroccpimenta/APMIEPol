b6aa94a2361706f77d296d6e9c5d7f37d1d97f9a
Pedro Correia Cravo Pimenta <pedroccpimenta@gmail.com>
2017-10-31 11:27:53 +0000

    Inclusão do cálculo no ciclo 'while'
    O programa pergunta repetidamente qual a geometria do novo cálculo.

--- Pratica5- geometria.py
+++ Pratica5- geometria.py
@@ -1,41 +1,60 @@
 '''Escrever um programa para calcular a área e o volume de uma esfera, um cilindro ou um cone,
 perguntando primeiro a geometria e, conforme a geometria, pedir as respectivas dimensões
-características.'''
+características.
 
+Porposta de resolução de Tatiana Pires
 
+'''
+
+from math import sqrt
+        
 
 geo = input ('Qual é a geometria? Esfera, Cilindro ou Cone ')
 geo = geo.lower()
 print(geo)
 
-#Como posso fazer para este ciclo while funcionar apenas quando a minha string tiver um erro ortografico (ou um carater que nao era suposto) ?
-'''while geo != ('esfera' or 'cilindro' or 'cone'):
+#Como posso fazer para este ciclo while funcionar apenas quando a minha string tiver um erro ortografico
+#(ou um carater que nao era suposto) ?
+
+# PCP - terá que incluir o cálculo (selectivo) 'dentro' do ciclo  no final, voltar a perguntar qual a
+# (nova) geometria
+
+# Altere o programa para indicar as unidades de medida do resultado (^2? ^3)
+# Altere o programa de forma a que o Utilizador indique 'fim' para terminar (sair do ciclo)
+
+
+while geo != ('esfera' or 'cilindro' or 'cone'):
     print ('A geometria nao está correta!')
     geo = input ('Qual é a geometria? Esfera, Cilindro ou Cone ')
     geo = geo.lower()
-    print(geo)''' 
+    print(geo)
 
-if geo == 'esfera':
-    raio = input ('Raio da esfera = ')
-    r = float(raio)
-    area = 4*3.14*(r**2)
-    volume = (4/3)*3.14*(r**3)
-    print ('Area =', area, 'Volume =', volume)
-elif geo == 'cilindro':
-    raio = input ('Raio da base do cilindro = ')
-    altura = input ('Altura do cilindro =')
-    r = float(raio)
-    a = float(altura)
-    area = 3.14*(r**2)*2 + 2*3.14*r*a
-    volume = 3.14*(r**2)*a
-    print ('Area =', area, 'Volume =', volume)
-elif geo == 'cone':
-    raio = input ('Raio da base do cone = ')
-    altura = input ('Altura do cone =')
-    r = float(raio)
-    a = float(altura)
-    from math import sqrt
-    g = sqrt(r**2 + a**2)
-    area = 3.14*(r**2) + 3.14*r*g
-    volume = (1/3)*3.14*(r**2)*a
-    print ('Area =', area, 'Volume =', volume)
+    if geo == 'esfera':
+        raio = input ('Raio da esfera = ')
+        r = float(raio)
+        area = 4*3.14*(r**2)
+        volume = (4/3)*3.14*(r**3)
+        print ('Area =', area, 'Volume =', volume)
+    elif geo == 'cilindro':
+        raio = input ('Raio da base do cilindro = ')
+        altura = input ('Altura do cilindro =')
+        r = float(raio)
+        a = float(altura)
+        area = 3.14*(r**2)*2 + 2*3.14*r*a
+        volume = 3.14*(r**2)*a
+        print ('Area =', area, 'Volume =', volume)
+    
+    elif geo == 'cone':
+        raio = input ('Raio da base do cone = ')
+        altura = input ('Altura do cone =')
+        r = float(raio)
+        a = float(altura)
+        
+        g = sqrt(r**2 + a**2)
+        area = 3.14*(r**2) + 3.14*r*g
+        volume = (1/3)*3.14*(r**2)*a
+        print ('Area =', area, 'Volume =', volume)
+    
+    
+    geo = input ('Qual é a geometria? Esfera, Cilindro ou Cone ')
+    geo = geo.lower()
