62cb32fa6c127c602dcf30f5a9f49e380e640130
Pedro Correia Cravo Pimenta <pedroccpimenta@gmail.com>
2017-11-17 11:46:48 +0000

    Notas sobre um upload
    Re-definição de uma função para resolução de um exercício.

--- exemplo 6(funções).py
+++ exemplo 6(funções).py
@@ -3,15 +3,15 @@ def f(nome, idade):
     print(nome,'tem',idade,'anos')
     return
 
-
-
+# duas formas diferentes de invocar a função f() # Comentário PCP
 f('maria',18)
           
 nome="Maria"
 idade="20"
 f(nome, idade)
 
-#2
+
+#2 - re-definição da função f() # Comentário PCP
 def f(nome, idade):
     anonascimento=2017-idade
     
@@ -21,11 +21,14 @@ def f(nome, idade):
 anon=f('maria',18)
 print ("Ano de nascimento:", anon)
 
-#3
+
+
+#3 - re-re-definição da função f() # Comentário PCP
 def f(nome, idade):
     anonascimento=2017-idade
     print(nome,'tem',idade,'anos','nascida no ano de', anonascimento)
     return
+    
 f('maria',18)
 
 
