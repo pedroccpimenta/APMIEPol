2d8cf64284dceb5a24ef2e40d923404309da54ba
soniamachado <33150574+soniamachado@users.noreply.github.com>
2017-11-17 10:52:02 +0000

    Add files via upload

---
+++ exemplo 6(funções).py
@@ -0,0 +1,33 @@
+#1
+def f(nome, idade):
+    print(nome,'tem',idade,'anos')
+    return
+
+
+
+f('maria',18)
+          
+nome="Maria"
+idade="20"
+f(nome, idade)
+
+#2
+def f(nome, idade):
+    anonascimento=2017-idade
+    
+    print(nome,'tem',idade,'anos','nascida no ano de', anonascimento)
+    return anonascimento
+
+anon=f('maria',18)
+print ("Ano de nascimento:", anon)
+
+#3
+def f(nome, idade):
+    anonascimento=2017-idade
+    print(nome,'tem',idade,'anos','nascida no ano de', anonascimento)
+    return
+f('maria',18)
+
+
+
+          
