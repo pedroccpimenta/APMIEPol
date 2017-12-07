6c3cf03b53d61e46404fa48f2f838b18c00126b3
Pedro Correia Cravo Pimenta <pedroccpimenta@gmail.com>
2017-12-07 12:53:15 +0000

    Auto stash before merge of "master" and "origin/master"

--- exercicio 7.3.py
+++ exercicio 7.3.py
@@ -1,6 +1,11 @@
 #Sónia Machado
 import csv
-f= open('pautaNotas exercicio 7.3.csv', 'r+', enconding='utf-8')
+f= open('pautaNotas.csv', 'r+', encoding='utf-8')
+# PCP - não é 'enconding', é 'encoding'
+
+# PCP - para ler a 1ª linha - cabeçalho da tabela - sem tentar interpretar o conteúdo
+k=f.readline() 
+
 r=csv.reader(f)
 soma=0
 
@@ -14,3 +19,18 @@ print(' média dos alunos é:', media)
     
     
 print(soma/len(l))
+
+# PCP - deve imprimir os resultados aos poucos, para ter a certeza dos cálculos que está a efectuar:
+print ('soma:', soma)
+# PCP - Soma está bem calculada?
+
+#PCP - para dividir a soma pelo número de alunos, o que deveria usar?
+print ('len(l[2])',len(l[2]))
+# PCP - o que é o len - comprimento - de l[2] ?
+
+
+print ('len(l)',len(l))
+# PCP - o que é o len - comprimento - de l ?
+
+# Como pode chegar ao fim do ciclo for e conhecer o número de alunos na lista?
+
